2d2fb91c5eb15e6ffab1bb3beb79aba7ebc306ac
Sravan Sai Lanka <ssailank@qti.qualcomm.com>
2023-02-22 14:30:53 +0530

    Frontend

---
+++ src/app/components/add-expense/add-expense.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Expense } from 'src/app/models/expense';
+import { ExpenseService } from 'src/app/services/expense.service';
+
+@Component({
+  selector: 'app-add-expense',
+  templateUrl: './add-expense.component.html',
+  styleUrls: ['./add-expense.component.css']
+})
+export class AddExpenseComponent implements OnInit {
+
+  expense: Expense =  new Expense();
+  constructor(private _expenseService: ExpenseService,
+              private _router: Router,
+              private _activateRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    const isIdPresent = this._activateRoute.snapshot.paramMap.has('id');
+    if(isIdPresent){
+      const id = +this._activateRoute.snapshot.paramMap.get('id');
+        
+        this._expenseService.getExpense(id).subscribe(
+          data => {this.expense = data; console.log(data)}
+        )
+    }
+    //throw new Error('Method not implemented.');
+  }
+  saveExpense(){
+    this._expenseService.saveExpense(this.expense).subscribe(
+      data => {
+        console.log('response', data)
+        this._router.navigateByUrl("/expenses");
+      }
+    )
+  }
+
+  deleteExpense(id:number){
+    this._expenseService.deleteExpense(id).subscribe(
+      data => {
+        console.log('deleted responase', data);
+        this._router.navigateByUrl('/expenses');
+      }
+    )
+  }
+}
---
+++ src/app/services/expense.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Expense } from '../models/expense';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpenseService {
+
+  private getUrl: string = "http://localhost:8080/api/v1/expenses";
+  constructor(private _httpClient: HttpClient) { }
+
+  getExpenses(): Observable<Expense[]>{
+    return this._httpClient.get<Expense[]>(this.getUrl).pipe(
+      map((response: any) =>response)
+    )
+  }
+
+  saveExpense(expense: Expense): Observable<Expense> {
+    return this._httpClient.post<Expense>(this.getUrl,expense);
+  }
+
+  getExpense(id: number): Observable<Expense>{
+    return this._httpClient.get<Expense>(`${this.getUrl}/${id}`).pipe(
+      map(response => response)
+    )
+  }
+
+  deleteExpense(id:number): Observable<any>{
+    return this._httpClient.delete(`${this.getUrl}/${id}`, {responseType: 'text'});
+  }
+}
