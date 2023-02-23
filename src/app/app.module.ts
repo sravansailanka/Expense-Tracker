184f07c1d96be2a15bd3a36ec919fef4721b77c8
Sravan Sai Lanka <ssailank@qti.qualcomm.com>
2023-02-23 13:08:28 +0530

    Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
 import {FormsModule} from "@angular/forms"
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppComponent } from './app.component';
 import { ListExpensesComponent } from './components/list-expenses/list-expenses.component';
 import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routers:Routes = [
   {path: 'expenses', component: ListExpensesComponent},
@@ -25,7 +27,9 @@ const routers:Routes = [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routers),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
